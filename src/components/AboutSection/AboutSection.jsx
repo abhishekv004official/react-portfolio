9a5a34a369609f24f75f26fe7ef77211102652bd
Your Name <you@example.com>
2026-07-30 15:00:23 +0530

    day 14 addition and changes

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,20 @@
+import './AboutSection.css';
+ 
+function AboutSection() {
+  return (
+    <section id='about' className='about-section'>
+      <div className='about-wrapper container'>
+        <div className='about-image'>
+          <img src='/profile.jpg' alt='Abhishek v' />
+        </div>
+        <div className='about-copy'>
+          <h1>About Me</h1>
+          <p>Hi! I'm Abhishek v, a developer based in India building fast, accessible web experiences.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+ 
+export default AboutSection;
+ 
